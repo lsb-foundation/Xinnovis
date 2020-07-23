9e84bc7320aa9f00710517a3f4b0f0412ace1f37
Microbubu <microbubu@live.com>
2020-07-23 09:42:51 +0800

    Revert "提取出主分支"
    This reverts commit a1af597d28ff66a37d0789ac772da67ce97d083f.

---
+++ MFMCalibrationTool/ViewModels/StatusBarViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommonLib.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalibrationTool.ViewModels
+{
+    public class StatusBarViewModel : BindableBase
+    {
+        private string _status;
+        public string Status
+        {
+            get => _status;
+            set => SetProperty(ref _status, value);
+        }
+
+        public void ShowStatus(string message)
+        {
+            Task.Run(async () =>
+            {
+                _status = message;
+                RaiseProperty(nameof(Status));
+                await Task.Delay(2000);
+                _status = string.Empty;
+                RaiseProperty(nameof(Status));
+            });
+        }
+    }
+}
