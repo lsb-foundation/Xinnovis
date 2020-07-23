61adbfe070b118930d7998f5c39b7c677d935b6d
Microbubu <microbubu@live.com>
2020-07-23 10:49:04 +0800

    初步创建指令配置项

--- CalibrationTool/Utils/ConfigManager.cs
+++ CalibrationTool/Utils/ConfigManager.cs
@@ -26,6 +26,7 @@ namespace CalibrationTool.Utils
             _config.Save();
         }
 
+        //保存默认值
         private static Dictionary<string,string> _defaultSettings = new Dictionary<string, string>()
         {
             { nameof(Serial_BaudRate), "115200" }
@@ -45,16 +46,35 @@ namespace CalibrationTool.Utils
             _config.Save();
         }
 
+        public static void Intialize()
+        {
+
+        }
+
+        #region 串口配置项
         public static string Serial_PortName
         {
             get => Read();
             set => Write(value);
         }
-
         public static int Serial_BaudRate
         {
             get => int.Parse(Read());
             set => Write(value.ToString());
         }
+        #endregion
+
+        #region 指令配置项
+        public static string CaliFlowVCommand { get => Read(); }         //标定流量电压的指令
+        public static string CaliVoltCommandHeader { get => Read(); }   //标定电压的指令头部
+        public static string CaliKCommandHeader { get => Read(); }      //标定斜率的指令的头部
+        public static string CaliTCommandHeader { get => Read(); }      //标定温度的指令的头部
+        public static string DebugCommand { get => Read(); }        //Debug指令
+        public static string AVStartCommandHeader { get => Read(); }    //开始输出1-5V电压的指令头部
+        public static string AVStopCommand { get => Read(); }       //停止输出1-5V电压的指令
+        public static string CheckCommandHeader { get => Read(); }  //检查实际输出电压的指令头部
+        public static string CheckStopCommand { get => Read(); }    //停止检查实际输出电压的指令
+        public static string AVCommandHeader { get => Read(); }     //写入标定1-5V的斜率K和截距D
+        #endregion
     }
 }
