584292f255faa0f8a831179434ae9e7d7f21aa0c
Microbubu <microbubu@live.com>
2020-07-27 18:03:43 +0800

    添加Debug相关信息

---
+++ CalibrationTool/Models/BaudRateCode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalibrationTool.Models
+{
+    public class BaudRateCode
+    {
+        public int Code { get; set; }
+        public int BaudRate { get; set; }
+
+        public BaudRateCode(int code, int baudRate)
+        {
+            Code = code;
+            BaudRate = baudRate;
+        }
+
+        private static int[] baudRates = new int[]
+        {
+            4800,9600,14400,19200,38400,56000,57600,
+            115200,128000,230400,256000,460800,500000
+        };
+
+        public static List<BaudRateCode> GetBaudRateCodes()
+        {
+            List<BaudRateCode> baudRateCodes = new List<BaudRateCode>();
+            for(int index = 0; index < baudRates.Length; index++)
+            {
+                BaudRateCode code = new BaudRateCode(index + 1, baudRates[index]);
+                baudRateCodes.Add(code);
+            }
+            return baudRateCodes;
+        }
+
+        public static List<int> GetBaudRates()
+        {
+            return baudRates.ToList();
+        }
+    }
+}
--- CalibrationTool/ResolveUtils/DebugDataResolve.cs
+++ CalibrationTool/ResolveUtils/DebugDataResolve.cs
@@ -15,11 +15,23 @@ namespace CalibrationTool.ResolveUtils
             string[] lines = dataStr.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
             foreach(string line in lines)
             {
-                string[] splitData = line.Trim().Split(':');
-                if (splitData.Length ==2)
-                {
-                    KeyValuePair<string, string> keyValue = new KeyValuePair<string, string>(splitData[0], splitData[1]);
-                    result.Add(keyValue);
+                if(line.IndexOf(':') != -1)
+                {   //将冒号当作分隔符
+                    string[] splitData = line.Trim().Split(':');
+                    if (splitData.Length == 2)
+                    {
+                        KeyValuePair<string, string> keyValue = new KeyValuePair<string, string>(splitData[0], splitData[1]);
+                        result.Add(keyValue);
+                    }
+                }
+                else
+                {   //最后一个空格当作分隔符
+                    int lastSpaceIndex = line.LastIndexOf(' ');
+                    if(lastSpaceIndex != -1)
+                    {
+                        KeyValuePair<string, string> keyValue = new KeyValuePair<string, string>(line.Substring(0, lastSpaceIndex), line.Substring(lastSpaceIndex + 1));
+                        result.Add(keyValue);
+                    }
                 }
             }
 
