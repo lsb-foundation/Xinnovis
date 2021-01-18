f8fbed802fa22cd5458973a5f3f7056e1065da78
Microbubu <microbubu@live.com>
2021-01-18 20:29:47 +0800

    串口按序收发

--- MFCSoftware/Common/SerialPortInstance.cs
+++ MFCSoftware/Common/SerialPortInstance.cs
@@ -13,7 +13,7 @@ namespace MFCSoftware.Common
         private static SerialPort com;
         private static readonly ConcurrentQueue<byte> buffer = new ConcurrentQueue<byte>();
         private static SerialCommand<byte[]> currentCommand = null;
-        private static bool timeOut;
+        //private static bool timeOut;
         private const int waitTime = 100;
 
         //public static event Action<byte[]> DataHandler;
@@ -22,24 +22,24 @@ namespace MFCSoftware.Common
         {
             com = new SerialPort();
             com.DtrEnable = true;
-            com.DataReceived += Com_DataReceived;
+            //com.DataReceived += Com_DataReceived;
         }
 
-        private readonly static object syncObj = new object();
-        private static void Com_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            lock (syncObj)
-            {
-                int count = com.BytesToRead;
-                byte[] buff = new byte[count];
-                com.Read(buff, 0, count);
-                foreach (var item in buff)
-                {
-                    if (timeOut) break;
-                    buffer.Enqueue(item);
-                }
-            }
-        }
+        //private readonly static object syncObj = new object();
+        //private static void Com_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        //{
+        //    lock (syncObj)
+        //    {
+        //        int count = com.BytesToRead;
+        //        byte[] buff = new byte[count];
+        //        com.Read(buff, 0, count);
+        //        foreach (var item in buff)
+        //        {
+        //            if (timeOut) break;
+        //            buffer.Enqueue(item);
+        //        }
+        //    }
+        //}
 
         public static SerialPort GetSerialPortInstance()
         {
@@ -47,7 +47,7 @@ namespace MFCSoftware.Common
             {
                 com = new SerialPort();
                 com.DtrEnable = true;
-                com.DataReceived += Com_DataReceived;
+                //com.DataReceived += Com_DataReceived;
             }
             return com;
         }
@@ -59,12 +59,16 @@ namespace MFCSoftware.Common
                 if (!com.IsOpen)
                     com.Open();
 
-                if (timeOut)
-                    PreHandleTimeOut();
+                //if (timeOut)
+                //    PreHandleTimeOut();
+                while (!string.IsNullOrEmpty(com.ReadExisting()))
+                {
+                    Thread.Sleep(5);
+                }
 
                 com.Write(command.Command, 0, command.Command.Length);
                 currentCommand = command;
-                timeOut = false;
+                //timeOut = false;
             }
             catch(Exception e)
             {
@@ -72,16 +76,15 @@ namespace MFCSoftware.Common
             }
         }
 
-        //发送之前预处理超时
-        private static void PreHandleTimeOut()
-        {
-            com.ReadExisting();
-            while(buffer.Count > 0)
-            {
-                buffer.TryDequeue(out byte _);
-            }
-        }
-
+        //发送之前预处理
+        //private static void PreHandleTimeOut()
+        //{
+        //    com.ReadExisting();
+        //    while(buffer.Count > 0)
+        //    {
+        //        buffer.TryDequeue(out byte _);
+        //    }
+        //}
 
         /// <summary>
         /// 异步获取数据。在确定的等待时间内，如果获取到的数据长度不足，抛出TimeoutException异常。
@@ -89,35 +92,45 @@ namespace MFCSoftware.Common
         /// <returns></returns>
         public async static Task<byte[]> GetResponseBytes()
         {
-            using (System.Timers.Timer timer = new System.Timers.Timer(waitTime))
-            using (CancellationTokenSource cts = new CancellationTokenSource())
+            //using (System.Timers.Timer timer = new System.Timers.Timer(waitTime))
+            //using (CancellationTokenSource cts = new CancellationTokenSource())
+            //{
+            //    timer.AutoReset = false;
+            //    timer.Elapsed += (s, e) => cts.Cancel();
+
+            //    List<byte> datas = new List<byte>();
+            //    await Task.Run(() =>
+            //    {
+            //        timer.Start();
+            //        while (datas.Count < currentCommand.ResponseLength)
+            //        {
+            //            if (cts.IsCancellationRequested) break;
+
+            //            if (buffer.TryDequeue(out byte data))
+            //                datas.Add(data);
+            //        }
+            //    }, cts.Token);
+
+            //    timer.Stop();
+
+            //    if (datas.Count < currentCommand.ResponseLength)
+            //    {
+            //        timeOut = true;
+            //        throw new TimeoutException();
+            //    }
+
+            //    return datas.ToArray();
+            //}  
+            await Task.Delay(waitTime);
+            if(com.BytesToRead < currentCommand.ResponseLength)
             {
-                timer.AutoReset = false;
-                timer.Elapsed += (s, e) => cts.Cancel();
-
-                List<byte> datas = new List<byte>();
-                await Task.Run(() =>
-                {
-                    timer.Start();
-                    while (datas.Count < currentCommand.ResponseLength)
-                    {
-                        if (cts.IsCancellationRequested) break;
-
-                        if (buffer.TryDequeue(out byte data))
-                            datas.Add(data);
-                    }
-                }, cts.Token);
-
-                timer.Stop();
-
-                if (datas.Count < currentCommand.ResponseLength)
-                {
-                    timeOut = true;
-                    throw new TimeoutException();
-                }
-
-                return datas.ToArray();
-            }  
+                //timeOut = true;
+                throw new TimeoutException();
+            }
+            int count = com.BytesToRead;
+            byte[] rets = new byte[count];
+            com.Read(rets, 0, count);
+            return rets;
         }
     }
 }
