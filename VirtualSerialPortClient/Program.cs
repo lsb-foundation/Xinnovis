b9ecc840cb1752b98779e725d159855f83bfdcda
Microbubu <microbubu@live.com>
2021-02-25 20:13:16 +0800

    模拟DEBUG!

--- VirtualSerialPortClient/Program.cs
+++ VirtualSerialPortClient/Program.cs
@@ -3,6 +3,7 @@ using System;
 using CommonLib.Extensions;
 using System.Threading;
 using System.Configuration;
+using System.Text;
 
 namespace VirtualSerialPortClient
 {
@@ -44,14 +45,16 @@ namespace VirtualSerialPortClient
             Console.WriteLine($"{portName} received: {buffer.ToHexString()}");
             Thread.Sleep(10);
 
-            if (buffer.Length != 8)
-                return;
+            //if (buffer.Length != 8)
+            //    return;
 
             byte[] dataToSend;
             if (buffer[3] == 0x16 && buffer[4] == 0x00 && buffer[5] == 0x0B)
                 dataToSend = GetFlow(buffer[0]);
             else if (buffer[3] == 0x03 && buffer[4] == 0x00 && buffer[5] == 0x10)
                 dataToSend = GetBaseInfo(buffer[0]);
+            else if (Encoding.Default.GetString(buffer) == "DEBUG!")
+                dataToSend = GetDebugData();
             else dataToSend = null;
 
             if (dataToSend != null)
@@ -117,6 +120,29 @@ namespace VirtualSerialPortClient
             ret[25] = crc[0];
             ret[26] = crc[1];
             return ret;
+        }
+
+        private static byte[] GetDebugData()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"SN:TESTSN001\r\n")
+                .Append($"Type of GAS:O2\r\n")
+                .Append($"Range:100\r\n")
+                .Append($"UNIT:SCCM\r\n")
+                .Append($"modbus addr:1\r\n")
+                .Append($"modbus baud:9600\r\n")
+                .Append($"K of 4-20mA:2.98\r\n")
+                .Append($"D of 4-20mA:19.90\r\n")
+                .Append($"K of 1-5V:8.67\r\n")
+                .Append($"D of 1-5V:29.77\r\n")
+                .Append($"T of cali flow:46\r\n")
+                .Append($"T of now:23\r\n")
+                .Append($"GAS FACTOR:18.54\r\n")
+                .Append($"P:6.87\r\n")
+                .Append($"I:8.78\r\n")
+                .Append($"D:6.54\r\n")
+                .Append($"Zero:7.34\r\n");
+            return Encoding.Default.GetBytes(builder.ToString());
         }
     }
 }
